44ed4bf2648e7ea39954c9298adc57b116e48f31
Catherine Tung <catt.tung@gmail.com>
2022-06-09 21:59:26 -0400

    function to display data

--- js/app.js
+++ js/app.js
@@ -6,13 +6,18 @@
 
 
 // variables
-
+let spreadsheetData
 // functions
 //api call to fetch data from json file
 fetch('../data.json')
 .then(response => {
   return response.json();
 })
-.then(data => console.log(data))
+.then(data => displayData(data))
 
-//
+// display the data
+function displayData(data) {
+  for (let i=0; i < data.length; i++) {
+    console.log(data[i].amount)
+  }
+}
